9fed2661d20a3dae1c28b5fd7481bd8be00bf400
Mahfuj A <mahfuj.ahbab@gmail.com>
2025-07-02 11:45:47 -0400

    new neovim config layout to support multiple configs

--- aliases/core.zsh
+++ aliases/core.zsh
@@ -1,8 +1,10 @@
 alias python="python3"
-alias vim="nvim"
 alias pip="pip3"
 alias activate-pyenv="source ~/.venvs/mono/bin/activate"
 
 # replay functionality
 alias rcmd="replay_cmd"
 alias rcd="replay_cd"
+
+# neovim configs
+alias vim='XDG_CONFIG_HOME=~/dotfiles/config/nvim/main nvim'
