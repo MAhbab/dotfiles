b3032aaa25950a9f2e153bf32872515f2c0cbc62
MAhbab-DD <mahfuj.ahbab@doordash.com>
2025-10-13 10:04:46 -0400

    disabled codeium code assist on markdown files

--- config/nvim/main/nvim/lua/ftsettings.lua
+++ config/nvim/main/nvim/lua/ftsettings.lua
@@ -19,3 +19,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.expandtab = true
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+	vim.cmd("CodeiumDisable")
+	end
+})
